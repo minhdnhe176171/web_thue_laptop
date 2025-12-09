7ec0f61b6e83f476046da6e247d9d334d8346e95
nminh2331 <168155118+nminh2331@users.noreply.github.com>
2025-12-09 10:28:15 +0700

    add staff

--- web_thue_laptop/web_thue_laptop/obj/Debug/net8.0/web_thue_laptop.AssemblyInfo.cs
+++ web_thue_laptop/web_thue_laptop/obj/Debug/net8.0/web_thue_laptop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web_thue_laptop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e74f196fa6cad0b609adb5d4f6d6f4be84fc8e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f45f43b5e6656b0a4c0aaad9bdf2f19cda6d3693")]
 [assembly: System.Reflection.AssemblyProductAttribute("web_thue_laptop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web_thue_laptop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
